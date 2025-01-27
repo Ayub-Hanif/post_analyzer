2f02c2c73c0c5037c26449d2162bb196d2394a10
Ayub <ayubhanif06@gmail.com>
2025-01-27 00:28:31 -0800

    shell classes added for now.

---
+++ src/main/java/com/ecs160/hw1/Json_parser_file.java
@@ -0,0 +1,9 @@
+package com.ecs160.hw1;
+import java.util.List;
+
+public class Json_parser_file {
+    public List<Post> json_parser(String filePath)
+    {
+        return List.of();
+    }
+}
---
+++ src/main/java/com/ecs160/hw1/Post.java
@@ -0,0 +1,4 @@
+package com.ecs160.hw1;
+public class Post {
+    public Post(int post_Id, String content, int creation_date, int word_count){}
+}
