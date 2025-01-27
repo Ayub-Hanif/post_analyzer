89ac23653dec61a9aa4a99927f8f8bc8d7a44a19
Akilan Babu <wertyyyy102@gmail.com>
2025-01-27 15:16:59 -0800

    Add Analyzer, Post classes

--- src/main/java/com/ecs160/hw1/Post.java
+++ src/main/java/com/ecs160/hw1/Post.java
@@ -1,4 +1,34 @@
 package com.ecs160.hw1;
+
+import java.util.Date;
+import java.util.List;
+
 public class Post {
-    public Post(int post_Id, String content, int creation_date, int word_count){}
+    private final int post_id;
+    private final int word_count;
+    private final Date creation_date;
+    private final List<Post> replies;
+
+    public Post(int post_id, int word_count, Date creation_date, List<Post> replies) {
+        this.post_id = post_id;
+        this.word_count = word_count;
+        this.creation_date = creation_date;
+        this.replies = replies;
+    }
+
+    public int getPost_id() {
+        return post_id;
+    }
+
+    public int getWord_count() {
+        return word_count;
+    }
+
+    public Date getCreation_date() {
+        return creation_date;
+    }
+
+    public List<Post> getReplies() {
+        return replies;
+    }
 }
